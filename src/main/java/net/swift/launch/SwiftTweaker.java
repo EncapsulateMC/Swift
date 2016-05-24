d3dac411242106f6acf7f04576a9286cda67da0e
PizzaCrust <torchprojectserver@gmail.com>
2016-05-24 18:48:04 +0700

    tweaker

---
+++ src/main/java/net/swift/launch/SwiftTweaker.java
@@ -0,0 +1,32 @@
+package net.swift.launch;
+
+import net.minecraft.launchwrapper.ITweaker;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+
+import java.io.File;
+import java.util.List;
+
+public class SwiftTweaker implements ITweaker {
+    @Override
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {}
+
+    @Override
+    public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+        classLoader.registerTransformer(ServerTransformer.class.getName());
+        try {
+            classLoader.findClass("net.minecraft.server.MinecraftServer");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getLaunchTarget() {
+        return "net.minecraft.server.MinecraftServer";
+    }
+
+    @Override
+    public String[] getLaunchArguments() {
+        return new String[] { "nogui" };
+    }
+}
